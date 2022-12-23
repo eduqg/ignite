e0b131ea101bb53c85e9dd18ad7cc126e2972bbe
eduqg <eduardoqgomes@gmail.com>
2022-12-23 09:28:15 -0300

    Initial structure to class ignitegym

---
+++ react-native-2022/aulas/3-ignitegym/App.tsx
@@ -0,0 +1,18 @@
+import { StatusBar, Text, View } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <View >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Text>Open up App.tsx to start working on your app!</Text> : <View />}
+    </View>
+  );
+}
+
